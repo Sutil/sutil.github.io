5e5b51b497e79cd57803d4d1d1f5f51f1cdbec6d
Eduardo <sutil.edu@gmail.com>
2019-09-09 14:55:38 -0300

    new version

---
+++ precache-manifest.c9d5db794ac9892664516e6405acb558.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f98c6949739f44af70fef199fb3c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6440c021a3d6f00e9297",
+    "url": "/static/js/2.cb1ba332.chunk.js"
+  },
+  {
+    "revision": "74bf72880c9791334ab3",
+    "url": "/static/js/main.70315824.chunk.js"
+  },
+  {
+    "revision": "bcba3d3fcc76351cea08",
+    "url": "/static/js/runtime~main.48df8006.js"
+  }
+]);
